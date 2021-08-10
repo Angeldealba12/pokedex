c160bd0f65c6504d9690a0e81d783872081728d2
Angel <angel.dealba.garcia@gmail.com>
2021-08-10 16:53:25 -0500

    stylishing my pokedex component

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,14 +1,28 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../context/userContext'
-
+import InputIcon from '@material-ui/icons/Input';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import '../css/pokedex.css'
 
 const Pokedex = () => {
 
-    const {user} =  useContext(UserContext)
+    const {user, setUser} =  useContext(UserContext)
+
+    const handleLogOut = () => {
+       setUser(null);
+    }
 
     return (
-       <div>
-           {user}
+       <div className="pokedex-container">
+           <nav className="pokedex-navtab">
+            <div className="pokedex-logo">
+                <AccountCircleIcon />
+            </div>
+            <h1>Welcome Trainer,  {user}</h1>
+            <div class="button-logOut">
+            <button onClick={handleLogOut}><InputIcon /></button>
+            </div>
+           </nav>
        </div> 
     )
 }
